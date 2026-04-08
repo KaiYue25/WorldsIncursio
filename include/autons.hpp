db311b68c27f0a4c96d623143dbdcea10cff86ef
neriri <nerielmadriaga12@gmail.com>
2026-04-08 18:39:48 +0800

    24Inch

---
+++ include/autons.hpp
@@ -0,0 +1,29 @@
+#pragma once
+//Functions
+void default_constants();
+void calibrateArms();
+void leverState();
+void discoreState();
+
+
+//Optical
+void grab_blue_reject_red();
+void grab_red_reject_blue();
+
+
+void drive_example();
+void turn_example();
+void drive_and_turn();
+void wait_until_change_speed();
+void swing_example();
+void motion_chaining();
+void combining_movements();
+void interfered_example();
+void odom_drive_example();
+void odom_pure_pursuit_example();
+void odom_pure_pursuit_wait_until_example();
+void odom_boomerang_example();
+void odom_boomerang_injected_pure_pursuit_example();
+void measure_offsets();
+
+
---
+++ include/subsystems.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+// Optical
+extern pros::Optical color_sensor;
+
+// Motors 
+inline pros::Motor intake(-3);
+inline pros::Motor lever(-2);
+inline pros::Motor discore(1);
+
+
+// Pneumatics
+inline ez::Piston matchLoad('F');
+inline ez::Piston lift('H');
+inline ez::Piston gate('G');
+
+enum LeverMode {LEVER_IDLE, LEVER_SLOW, LEVER_FAST};
+extern Drive chassis;
+extern LeverMode auto_lever_mode;
+inline LeverMode auto_lever_mode = LEVER_IDLE; 
+inline bool is_testing_auton = false;
+
+// Lever Class
+class LeverController {
+public:
+    // We add a "timeout" variable. By default, it gives the arm time  to finish.
+    void fast(int timeout = 1000) {
+        auto_lever_mode = LEVER_FAST; 
+        int time_spent = 0;
+        
+        // Loop runs AS LONG AS it's under 85 degrees AND we haven't run out of time
+        while (lever.get_position() < 85.0 && time_spent < timeout) {
+            pros::delay(20);
+            time_spent += 20; // Add 20ms to our stopwatch every loop
+        }
+    }
+
+    void slow(int timeout = 2000) {
+        auto_lever_mode = LEVER_SLOW; 
+        int time_spent = 0;
+        
+        while (lever.get_position() < 85.0 && time_spent < timeout) {
+            pros::delay(20);
+            time_spent += 20;
+        }
+    }
+
+    void down(int timeout = 2000) {
+        auto_lever_mode = LEVER_IDLE; 
+        int time_spent = 0;
+        
+        while (lever.get_position() > 10.0 && time_spent < timeout) {
+            pros::delay(20);
+            time_spent += 20;
+        }
+    }
+};
+
+inline LeverController fireLever;
